cc7c7f07c1bc337390dcd8fb4bc4d749cd68eded
GodotMisogi <GodotMisogi@bitbucket.org>
2020-12-18 02:06:24 +0800

    Added TLIP Presentation

---
+++ notebooks/TLIPPresentation.jl
@@ -0,0 +1,55 @@
+### A Pluto.jl notebook ###
+# v0.12.17
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 25d361d0-407a-11eb-2e31-e393bd8aa7e1
+md"""
+# TLIP - Aircraft Design
+"""
+
+# ╔═╡ 670c4350-408f-11eb-0ded-f7d2115bd86c
+md"""### Fixed Wing-VTOL Hybrid Case
+
+Example Case:
+![](https://godot-bloggy.xyz/DesignFramework.svg)
+
+"""
+
+# ╔═╡ e6e5cbe0-4090-11eb-355d-b59a4e83187c
+md"""## Why Julia?
+
+1. It's fast.
+2. Similar to MATLAB, but free and faster.
+	> Students and instructors are familiar with MATLAB.
+3. Has Pluto, this nice notebook format for running experiments and giving instructive presentations.
+"""
+
+# ╔═╡ 2a2ab690-4091-11eb-0c34-89a03ac496c5
+md"""## Progress
+
+1. Selection of language for development.
+2. Development of notebooks for experiments in MECH 3620 -- Aircraft Design.
+3. Development of initial weight estimation code.
+4. Development of constraint analysis code. 
+5. Development of tail sizing code.
+6. Development of doublet-source panel method code for 2D aerodynamics analyses (WIP).
+7. Development of vortex-lattice method code for 3D aerodynamics analyses (WIP).
+"""
+
+# ╔═╡ 067fe520-4092-11eb-2cb9-c70f29ceb3da
+md"""### To Do
+
+1. Formulate a 'simple' optimization problem for MECH 3620.
+2. Evaluate stability derivatives using vortex-lattice code for MECH ????.
+3. Develop code for performance evaluations of aircraft for MECH ????. 
+4. Develop a module for structural analyses of aircraft. 
+"""
+
+# ╔═╡ Cell order:
+# ╟─25d361d0-407a-11eb-2e31-e393bd8aa7e1
+# ╟─670c4350-408f-11eb-0ded-f7d2115bd86c
+# ╟─e6e5cbe0-4090-11eb-355d-b59a4e83187c
+# ╟─2a2ab690-4091-11eb-0c34-89a03ac496c5
+# ╟─067fe520-4092-11eb-2cb9-c70f29ceb3da
