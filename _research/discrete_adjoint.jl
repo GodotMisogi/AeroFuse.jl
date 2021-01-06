8dfca8da05e6a58d59a6b25a7d351f9f4313bb2b
GodotMisogi <GodotMisogi@github.com>
2021-01-06 21:24:44 +0800

    Newton solver on Laplace is working with Zygote

--- _research/discrete_adjoint.jl
+++ _research/discrete_adjoint.jl
@@ -3,87 +3,125 @@ using Zygote
 using StaticArrays
 using Base.Iterators
 
-##
+## Finite differencing
+central_2nd(Tm, T, Tp) = Tm - 2T + Tp
+central_2nd_diff(Tm, T, Tp, h) = central_2nd(Tm, T, Tp) / h^2
+central_1st_diff(Tm, Tp, h) = (Tm - Tp) / 2h
 
-# Finite differencing
-central_2nd(T) = T[1] - 2 * T[2] + T[3]
-central_2nd_diff(T, h) = central_2nd(T) / h^2
-# central_2nd_diff(T, h) = diff(diff(T, dims = 2), dims = 2) / h^2
-central_1st_diff(T, h) = (T[3] - T[1]) / (2h)^2
-
-# [β₁, β₂, T]
 compute_k(β₁, β₂, T) = β₁/T + β₂
 
 ∇compute_k(β₁, β₂, T) = gradient(compute_k, β₁, β₂, T)
 
 compute_q(x, y) = 5e5 * (1 - (x - 0.5)^2) * (1 - (y - 0.5)^2)
 
-function residual(Ts, βs, q, h)
-    T_row = SVector(Ts[2], Ts[1], Ts[3]) # T_i-1, j   T_i,j T_i+1,j
-    T_col = SVector(Ts[4], Ts[1], Ts[5])
-    k = compute_k(βs..., Ts[1]) 
-    ∂k∂T = ∇compute_k(βs..., Ts[1])[3]
-    
-    k * (central_2nd_diff(T_row, h) + central_2nd_diff(T_col, h)) + ∂k∂T * (central_1st_diff(T_row, h)^2 + central_1st_diff(T_col, h)^2) + q
+function get_T(T_grid, i, j, T_boundary)
+    if i < 1 || j < 1 || i > length(T_grid[:,1])|| j > length(T_grid[:,1])
+        T_boundary
+    else
+        T_grid[i,j]
+    end
 end
+
+stencil_1(T, i, j, T_boundary) = [ get_T(T, i, j, T_boundary), get_T(T, i-1, j, T_boundary), get_T(T, i+1, j, T_boundary), get_T(T, i, j-1, T_boundary), get_T(T, i, j+1, T_boundary) ]
+
+# Difference operators specific to stencil_1
+Δ²x(T, h) = central_2nd_diff(T[2], T[1], T[3], h)
+Δ²y(T, k) = central_2nd_diff(T[4], T[1], T[5], k)
+Δx(T, h) = central_1st_diff(T[3], T[2], h)
+Δy(T, k) = central_1st_diff(T[5], T[4], k)
+
+function residual(T, βs, q, h)
+    k = compute_k(βs..., first(T)) 
+    ∂k∂T = ∇compute_k(βs..., first(T))[3]
+    
+    k * (Δ²x(T, h) + Δ²y(T, h)) + ∂k∂T * (Δx(T, h)^2 + Δy(T, h)^2 ) + q
+end
+
+# function residual_boundary(T, T_boundary, βs, q, h)
+#     k = compute_k(βs..., first(T)) 
+#     ∂k∂T = ∇compute_k(βs..., first(T))[3]
+    
+# end
 
 ∇residual(Ts, βs, q, h) = gradient(residual, Ts, βs, q, h)
 
-function residual_discrete_k(Ts, ks, q, h)
-    T_row = SVector(Ts[2], Ts[1], Ts[3]) # T_i-1, j   T_i,j T_i+1,j
-    T_col = SVector(Ts[4], Ts[1], Ts[5])
-
-    k_row = SVector(ks[2], ks[1], ks[3]) # k_i-1, j   k_i,j k_i+1,j
-    k_col = SVector(ks[4], ks[1], ks[5])
-
-    k_row[1] * (central_2nd_diff(T_row, h) + central_2nd_diff(T_col, h)) +
-    central_1st_diff(k_row, h) * central_1st_diff(T_row, h) + central_1st_diff(k_col, h) * central_1st_diff(T_col, h) + q
-end
-
-∇residual_discrete_k(Ts, βs, q, h) = gradient(residual_discrete_k, Ts, βs, q, h)
-
-function update_k_grid_ghost_points!(k_grid)
-    k_grid[1, :] = 2 * k_grid[2, :] .- k_grid[3, :]
-    k_grid[end, :] = 2 * k_grid[end-1, :] .- k_grid[end-2, :]
-    k_grid[:, 1] = 2 * k_grid[:, 2] .- k_grid[:, 3]
-    k_grid[:, end] = 2 * k_grid[:, end-1] .- k_grid[:, end-2]
-
-    k_grid
-end
-
-@Zygote.adjoint (T :: Type{<:SVector})(xs :: Number ...) = T(xs...), dv -> (nothing, dv...)
-@Zygote.adjoint (T :: Type{<:SVector})(x :: AbstractVector) = T(x), dv -> (nothing, dv)
-
-
 ## Grid setup
-Δh = 0.05
+Δh = 0.2
 xs = 0:Δh:1
 grid = product(xs, xs)
 T_boundary = 200.
-βs = [ 0.1, 0.5 ]
+βs = [13221, 27]
+ω = 0.8
 
 T₀ = T_boundary * ones(size(grid))
-q₀ = map(xs -> compute_q(xs...), grid)
+q₀ = map(xs -> compute_q(xs...), grid);
 
-stencil_1(T, i, j) = SVector(T[i, j], T[i-1, j], T[i+1, j], T[i, j-1], T[i, j+1])
+n = length(xs)
+T = deepcopy(T₀)
+R = zeros(size(T)...)
+∂R∂T = zeros(n, n, n, n)
+num_iters = 15
+ε = zeros(num_iters)
 
-R = [  
-        let Ts = stencil_1(T₀, indices...);
-        residual(Ts, βs, q₀[indices...], Δh) end
-        for indices in product(2:length(xs)-1, 2:length(xs)-1) 
-    ]
+## Iteration
+for i in 1:num_iters
+    for R_ind in CartesianIndices(R)
+        k, l = R_ind.I
+        Ts = stencil_1(T, k, l, T_boundary)
+        
+        # Residual 
+        R[R_ind] = residual(Ts, βs, q₀[R_ind], Δh)
+        
+        # Jacobian
+        grad = ∇residual(Ts, βs, q₀[R_ind], Δh)[1]
 
-∇R = 
-    [  
-        let Ts = stencil_1(T₀, indices...);
-        ∇residual(Ts, βs, q₀[indices...], Δh)[1] end
-        for indices in product(2:length(xs)-1, 2:length(xs)-1) 
-    ]
+        ∂R∂T[R_ind,R_ind] = grad[1]
+        if k > 1 ∂R∂T[R_ind,k-1,l] = grad[2] end
+        if k < n ∂R∂T[R_ind,k+1,l] = grad[3] end
+        if l > 1 ∂R∂T[R_ind,k,l-1] = grad[4] end
+        if l < n ∂R∂T[R_ind,k,l+1] = grad[5] end
+    end
+    
+    # display(T)
+    # display(R)
+    # display(reshape(∂R∂T, size(T).^2))
 
-# ∂R∂T = ???
+    # Solve system
+    ΔT = - reshape(reshape(∂R∂T, size(T).^2) \ R[:], size(T)) 
+    ε[i] = maximum(abs.(ΔT))
+    T .+= ω * ΔT
+end
+
+##
+using Plots
+plotlyjs()
+
+##
+plot(ε)
+
+##
+contourf(xs, xs, q₀)
+
+##
+contourf(xs, xs, T)
 
 # begin
 #     ΔT = ∂R∂T \ -ΔR
 #     T₀ += ω * reshape(ΔT, size(T₀))
 #     residual = max(abs.(ΔT))
+# end#
+
+
+# residual_discrete_k(T, k, q, h) = 
+#     first(k) * (Δ²x(T) + Δ²y(T)) / h^2 + (Δx(k) / h) * (Δx(T) / h) + (Δy(k) / h) * (Δy(T) / h) + q
+
+# ∇residual_discrete_k(Ts, βs, q, h) = gradient(residual_discrete_k, Ts, βs, q, h)
+
+# function update_k_grid_ghost_points!(k_grid)
+#     k_grid[1,:] = 2 * k_grid[2,:] .- k_grid[3,:]
+#     k_grid[end,:] = 2 * k_grid[end-1,:] .- k_grid[end-2,:]
+#     k_grid[:,1] = 2 * k_grid[:,2] .- k_grid[:,3]
+#     k_grid[:,end] = 2 * k_grid[:,end-1] .- k_grid[:,end-2]
+
+#     k_grid
 # end
