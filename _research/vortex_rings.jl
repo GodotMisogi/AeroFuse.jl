fb8adca398ab0432282e1bea3fc99ac82ded56e1
GodotMisogi <GodotMisogi@github.com>
2021-06-15 02:26:04 +0800

    Updated constructor

--- _research/vortex_rings.jl
+++ _research/vortex_rings.jl
@@ -1,18 +1,19 @@
 """
 A vortex ring type consisting of vortex lines. TODO: Consider if better fields are "bound_vortices" and "trailing_vortices"
 """
-struct VortexRing <: AbstractVortexArray
-    left_leg :: Line
-    bound_leg :: Line
-    back_leg :: Line
-    right_leg :: Line
+struct VortexRing{T} <: AbstractVortexArray
+    left_leg  :: Line{T}
+    bound_leg :: Line{T}
+    back_leg  :: Line{T}
+    right_leg :: Line{T}
 end
 
 """
 Helper function to compute the vortex ring given four points following Panel3D ordering.
 """
 function vortex_ring(p1, p2, p3, p4)
-    v1, v4 = quarter_point(p1, p2), quarter_point(p4, p3)
+    v1 = quarter_point(p1, p2)
+    v4 = quarter_point(p4, p3)
     v2 = @. v1 + p2 - p1
     v3 = @. v4 + p3 - p4
     
@@ -35,15 +36,15 @@ left_line       right_line
     p2 —back_line→ p3
 ```
 """
-function VortexRing(panel :: Panel3D)
+function VortexRing(panel :: Panel3D{T}) where T <: Real
     v1, v2, v3, v4 = vortex_ring(panel)
 
     bound_leg = Line(v1, v4)
-    left_leg = Line(v2, v1)
+    left_leg  = Line(v2, v1)
     right_leg = Line(v3, v2)
-    back_leg = Line(v4, v3)
+    back_leg  = Line(v4, v3)
 
-    VortexRing(left_leg, bound_leg, back_leg, right_leg)
+    VortexRing{T}(left_leg, bound_leg, back_leg, right_leg)
 end
 
 """
