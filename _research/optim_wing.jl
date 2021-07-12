3cef85e42eaac6c1fa0748aea46296f4693a478f
Arjit Seth <ajseth@ust.hk>
2021-07-13 01:24:56 +0800

    Cleaned up with more tests

--- _research/optim_wing.jl
+++ _research/optim_wing.jl
@@ -1,85 +1,123 @@
-## 
+## Optimisation tests
 using Revise
 using StaticArrays
 using AeroMDAO
 using Optim
 
+## FUNCTIONAL TEST
+#==========================================================================================#
+
 # Helper functions
 #============================================#
 
-make_wing(chords, twists, spans, dihedrals, sweeps) = Wing(chords = chords, twists = twists, spans = spans, dihedrals = dihedrals, sweep_LEs = sweeps)
-
-function run_case(wing, V, α)
+function run_case(wing, V, α, ρ, span_num, chord_num)
     uniform = Freestream(V, α, 0.0, zeros(3))
-    ref = SVector(0.25 * mean_aerodynamic_chord(wing), 0., 0.)
-    nf_coeffs, ff_coeffs, horseshoe_panels, normals, horseshoes, Γs = solve_case(wing, uniform, rho_ref = 1.225, r_ref = ref, span_num = 20, chord_num = 13)
-
-    ff_coeffs
+    coeffs  = solve_case(wing, uniform, 
+                         rho_ref = ρ,
+                         span_num = span_num,
+                         chord_num = chord_num)[2]
 end
 
 # Objective function
-evaluate_CDi(wing, V, α) = run_case(wing, V, α)[1]
+evaluate_CDi(wing, V, α, ρ, span_num, chord_num) = run_case(wing, V, α, ρ, span_num, chord_num)[1]
 
 # Lift and area constraint function
-function cons_prob(wing, V, α, ρ = 1.225)
-    S    = projected_area(wing)
-    L    = dynamic_pressure(ρ, V) * S * run_case(wing, V, α)[3] 
+function evaluate_cons(wing, V, α, ρ, span_num, chord_num)
+    area = projected_area(wing)
+    lift = dynamic_pressure(ρ, V) * S * run_case(wing, V, α, ρ, span_num, chord_num)[3]
 
-    L, S
+    lift, area
 end
 
 ## Test runs
 #============================================#
 
 # Parameters
-wing = Wing(chords    = [1.0, 0.5, 0.4, 0.3, 0.2],
-            twists    = [0., 0., 0., 0., 0.],
-            spans     = fill(2.6 / 4, 4),
-            dihedrals = [0., 0., 0., 0.],
-            sweep_LEs = [0., 0., 0., 0.])
+n    = 4
+wing = Wing(foils     = fill(Foil(naca4((2,4,1,2))), n),
+            chords    = fill(0.3, n),
+            twists    = fill(0.0, n),
+            spans     = fill(1.3/(n-1), n-1),
+            dihedrals = fill(0.0, n-1),
+            sweep_LEs = fill(10., n-1))
 
-V         = 20.
-α         = 3.
+wing = WingSection(root_foil  = naca4((2,4,1,2)),
+                   tip_foil   = naca4((2,4,1,2)),
+                   span       = 2.6,
+                   root_chord = 0.314,
+                   taper      = 0.8,
+                   root_twist = 0.0,
+                   tip_twist  = 0.0,
+                   dihedral   = 5.0,
+                   sweep_LE   = 10.)
+
+# Meshing and assembly
+wing_mac = mean_aerodynamic_center(wing);
+b, S, c  = info(wing)[1:end-1]
+V, α, ρ  = 30., 4., 1.225
+
+span_num  = 10
+chord_num = 6
 
 # Test case
-ff_coeffs = @time run_case(wing, V, α)
-cons_test = @time cons_prob(wing, V, α, 1.225)
+ff_coeffs = @time evaluate_CDi(wing, V, α, ρ, span_num, chord_num)
+cons_test = @time evaluate_cons(wing, V, α, ρ, span_num, chord_num)
 
 ## Optimisation
 #============================================#
 
 # Design variables - chords and angle of attack
-x0           = [chords(wing); α]
-weight       = 12 * 9.81
+x0           = [(chords ∘ right)(wing); α]
+
+x0           = [mean_aerodynamic_chord(wing);
+                taper_ratio(right(wing));
+                dihedrals(right(wing));
+                sweeps(right(wing));
+                α]
+
+weight       = 15 * 9.81
 load_factor  = 1.5
 lift_req     = weight * load_factor
 
 # Bounds
 l_bound      = fill(1e-12, length(x0))
-u_bound      = fill(Inf, length(x0))
-bound_chords = TwiceDifferentiableConstraints(l_bound, u_bound)
+u_bound      = fill(Inf,   length(x0))
 
 # Constraint bounds
-lc           = [ lift_req, projected_area(wing) ]
-uc           = [ lift_req, projected_area(wing) ]
+lc = [ lift_req, projected_area(wing) ]
+uc = [ lift_req, projected_area(wing) ]
 
 # Closures
-make_wing(x)      = make_wing(x, twists(wing), spans(wing), dihedrals(wing), sweeps(wing))
-evaluate_CDi(x)   = evaluate_CDi(make_wing(x[1:end-1]), V, x[end])
-cons_prob!(cs, x) = cs .= cons_prob(make_wing(x[1:end-1]), V, x[end])
+make_wing(x) = Wing(chords    = x, 
+                    foils     = foils(right(wing)),
+                    spans     = spans(right(wing)),
+                    twists    = rad2deg.(twists(right(wing))), 
+                    dihedrals = rad2deg.(dihedrals(right(wing))),
+                    sweep_LEs = rad2deg.(sweeps(right(wing))))
+
+make_wing(x) = WingSection(root_foil  = naca4((2,4,1,2)),
+                           tip_foil   = naca4((2,4,1,2)),
+                           span       = span(wing),
+                           root_chord = x[1],
+                           taper      = x[2],
+                           dihedral   = rad2deg(x[3]),
+                           sweep_LE   = rad2deg(x[4]))
+
+evaluate_CDi(x) = evaluate_CDi(make_wing(x[1:end-1]), V, x[end], ρ, span_num, chord_num)
+cons!(cs, x) = cs .= evaluate_cons(make_wing(x[1:end-1]), V, x[end], ρ, span_num, chord_num)
 
 # Optim variables
 optimize_chords = TwiceDifferentiable(evaluate_CDi, x0)
-lift_constraint = TwiceDifferentiableConstraints(cons_prob!, l_bound, u_bound, lc, uc)
+cons_lift_area  = TwiceDifferentiableConstraints(cons!, l_bound, u_bound, lc, uc);
 
-# Run optimisation
+## Run optimisation
 res_chord = optimize(optimize_chords,          # Objective functions
-                     lift_constraint,          # Constraint
+                     cons_lift_area,           # Constraint
                      x0,                       # Initial value
                      IPNewton(),               # Optimization algorithm
                     #  autodiff = :forward,    # Automatic differentiation
                      Optim.Options(
-                                   # extended_trace = true,
+                                #    extended_trace = true,
                                    show_trace = true
                                   )
                     )
@@ -90,52 +128,24 @@ res_chord = optimize(optimize_chords,          # Objective functions
 using Plots
 pyplot(dpi = 300)
 
-opt_wing  = make_wing(res_chord.minimizer[1:end-1], twists(wing), spans(wing), dihedrals(wing), sweeps(wing))
-wing_plan = plot_wing(opt_wing)
+ini_plan = plot_wing(wing)
+
+opt_wing = make_wing(res_chord.minimizer[1:end-1])
+opt_plan = plot_wing(opt_wing)
 
 b = span(opt_wing)
 plot(xlim = (-b/2, b/2), zlim = (-b/2, b/2))
-plot!(wing_plan, color = :blue, label = :none)
-
+plot!(ini_plan, label = "Initial")
+plot!(opt_plan, color = :blue, label = "Optimized")
 
 ## STATEFUL TEST
 #==========================================================================================#
 
-# Lifting surfaces setup
-#==========================================================================================#
-
-# Define wing
-wing = Wing(foils     = Foil.(fill(naca4((0,0,1,2)), 2)),
-            chords    = [1.0, 0.6],
-            twists    = [2.0, 2.0],
-            spans     = [5.0],
-            dihedrals = [11.3],
-            sweep_LEs = [2.29]);
-
-## Meshing and assembly
-wing_panels, wing_normals = panel_wing(wing,  20, 13;)
-aircraft = Dict("Wing" => (wing_panels,  wing_normals));
-
-## System setup
-#==========================================================================================#
-
-# Set up state
-wing_mac = mean_aerodynamic_center(wing);
-state = VLMState(1., 0., 0., [0., 0., 0.], 
-                 rho_ref   = 1.225,
-                 r_ref     = [ wing_mac[1], 0, 0 ],
-                 area_ref  = projected_area(wing), 
-                 chord_ref = mean_aerodynamic_chord(wing), 
-                 span_ref  = span(wing));
-
-# Set up system
-system = solve_case(aircraft, state);
-
 # Helper functions
 #============================================#
 
-function make_surface(bing, span_num, chord_num)
-    wing_panels, wing_normals = panel_wing(bing, span_num, chord_num;)
+function make_surface(wing, span_num, chord_num)
+    wing_panels, wing_normals = panel_wing(wing, span_num, chord_num;)
     VLMSurface(wing_panels, wing_normals, "Wing")
 end
 
@@ -157,26 +167,30 @@ evaluate_constraints(wing, system) = [ projected_area(wing); sum(farfield_forces
 ## Optimisation
 #============================================#
 
-# Test case - Fixed speed
-weight        = 12 * 9.81
-load_factor   = 1.5
-state.alpha   = deg2rad(3.)
-state.rho_ref = 1.1
+# Set up state
+state = VLMState(V, deg2rad(α), 0., [0., 0., 0.], 
+                 rho_ref   = ρ,
+                 r_ref     = [ wing_mac[1], 0, 0 ],
+                 area_ref  = projected_area(wing), 
+                 chord_ref = mean_aerodynamic_chord(wing), 
+                 span_ref  = span(wing));
 
-# Design variables - chords and angle of attack
-c0        = chords(wing)
-α0        = state.alpha
-x0        = [c0; α0]
-span_num  = 20
-chord_num = 13
+# Solve initial case
+aircraft = Dict("Wing" => panel_wing(wing, span_num, chord_num));
+system   = solve_case(aircraft, state);
+
+# Test case - Fixed speed
+state.speed   = V
+state.alpha   = deg2rad(α)
+state.rho_ref = ρ
 
 # Bounds
 l_bound = fill(1e-12, length(x0))
 u_bound = fill(Inf, length(x0))
 
 # Constraint bounds
-lc = [ load_factor * weight, projected_area(wing)]
-uc = [ load_factor * weight, projected_area(wing)]
+lc = [ lift_req, projected_area(wing)]
+uc = [ lift_req, projected_area(wing)]
 
 # Closures for in-place assignment and parameters
 make_surface(wing) = make_surface(wing, span_num, chord_num)
@@ -197,7 +211,6 @@ end
 ## Optim variables
 optimize_chords = TwiceDifferentiable(obj_func!, x0)
 cons_lift_area  = TwiceDifferentiableConstraints(cons_func!, l_bound, u_bound, lc, uc)
-# bound_chords    = TwiceDifferentiableConstraints(l_bound, u_bound)
 
 ## Evaluate case
 res_chord = optimize(optimize_chords,          # Objective functions
@@ -210,3 +223,16 @@ res_chord = optimize(optimize_chords,          # Objective functions
                                    show_trace = true
                                   )
                     )
+
+## Plotting
+#============================================#
+
+ini_plan = plot_wing(wing)
+
+opt_wing = make_wing(res_chord.minimizer[1:end-1])
+opt_plan = plot_wing(opt_wing)
+
+b = span(opt_wing)
+plot(xlim = (-b/2, b/2), zlim = (-b/2, b/2))
+plot!(ini_plan, label = "Initial")
+plot!(opt_plan, color = :blue, label = "Optimized")
