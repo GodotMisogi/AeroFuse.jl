45ab49f87df4255011559cbc04358b5c08e6fc5d
GodotMisogi <GodotMisogi@github.com>
2021-08-03 17:49:28 +0800

    More minor tests, most things appear to be correct

--- _research/connected_beam.jl
+++ _research/connected_beam.jl
@@ -10,11 +10,11 @@ using DataFrames
 # Define wing
 wing = WingSection(root_foil  = naca4(0,0,1,2),
                    span       = 3.11,
-                   dihedral   = 5.0,
-                   sweep_LE   = 20.0,
-                   taper      = 0.5,
-                   root_chord = 0.3,
-                   root_twist = 2.0,
+                   dihedral   = 0.0, # 5.0
+                   sweep_LE   = 0.0, # 20.0
+                   taper      = 1.0, # 0.5
+                   root_chord = 0.3, 
+                   root_twist = 0.0, # 2.0
                    tip_twist  = 0.0)
 wing_mac    = mean_aerodynamic_center(wing)
 wing_plan   = plot_wing(wing)
@@ -22,7 +22,7 @@ wing_name   = "Wing"
 print_info(wing, wing_name)
 
 # Mesh
-span_num        = 5
+span_num        = 1
 chord_num       = 2
 # xyzs            = chop_wing(coordinates(wing), [span_num], chord_num)
 panels, normies = panel_wing(wing, span_num, chord_num, spacing = "sine");
@@ -39,6 +39,7 @@ aero_state = VLMState(1., 0., 0., [0.0, 0.0, 0.0],
 # Test case - Fixed speed
 aero_state.speed   = 22.876
 aero_state.alpha   = deg2rad(5.)
+aero_state.beta    = deg2rad(0.)
 aero_state.rho_ref = 0.770816
 
 # Build system with initial guess from aerodynamic-only analysis
@@ -50,7 +51,7 @@ print_coefficients(aero_surfs[1], aero_state);
 #==========================================================================================#
 
 # Functions on adjacencies
-adjacent_joiner(x1, x2) = [ [ x1[1] ]; x1[2:end,:] .+ x2[1:end-1,:]; [ x2[end] ] ]
+adjacent_joiner(x1, x2) = [ [ x1[1] ]; x1[2:end] .+ x2[1:end-1]; [ x2[end] ] ]
 # AeroMDAO.VortexLattice.points(horses) = @. r1(bound_leg_center(horses), horses)[:], r2(bound_leg_center(horses), horses)[:]
 
 # Get variables for structural analysis
@@ -59,7 +60,7 @@ horsies  = horseshoes(aero_surfs[1])
 # r1s, r2s = points(horsies)
 
 fem_w       = 0.35
-plan_coords = coordinates(wing, span_num, chord_num)
+plan_coords = chord_coordinates(wing, [span_num], chord_num, spacings = ["sine"])
 mesh        = reshape(permutedims(reduce(hcat, plan_coords)), (size(plan_coords)..., 3))
 
 # Aerodynamic center locations and forces
@@ -69,35 +70,43 @@ ac_pts   = reshape(permutedims(reduce(hcat, ac_mine)), (size(ac_mine)..., 3))
 
 # FEM beam node locations as Matrix
 fem_arr  = (1 - fem_w) * mesh[1,:,:] + fem_w * mesh[end,:,:]
-M1s = sum(let xs = (ac_pts[i,:,:] - fem_arr[1:end-1,:]); 
-              @. SVector(xs[:,1], xs[:,2], xs[:,3]) × forces[i,:] / 2 end
-              for i in eachindex(ac_pts[:,1,1]))
-M2s = sum(let xs = (ac_pts[i,:,:] - fem_arr[2:end,:]); 
-              @. SVector(xs[:,1], xs[:,2], xs[:,3]) × forces[i,:] / 2 end
-              for i in eachindex(ac_pts[:,1,1]))
 
-## FEM beam node locations as Vector{SVector{3}}
-# fem_pts  = (1 - fem_w) * plan_coords[1,:] + fem_w * plan_coords[end,:]
+# Talk about a complicated...
+M_in  = [ let xs = (ac_pts[i,:,:] - fem_arr[1:end-1,:]); 
+              @. SVector(xs[:,1], xs[:,2], xs[:,3]) × forces[i,:] / 2 end
+              for i in eachindex(ac_pts[:,1,1]) ]
+M_out = [ let xs = (ac_pts[i,:,:] - fem_arr[2:end,:]); 
+              @. SVector(xs[:,1], xs[:,2], xs[:,3]) × forces[i,:] / 2 end
+              for i in eachindex(ac_pts[:,1,1]) ]
+
+M_ins  = sum(M_in)
+M_outs = sum(M_out) 
+
+# FEM beam node locations as Vector{SVector{3}}
+fem_pts  = (1 - fem_w) * plan_coords[1,:] + fem_w * plan_coords[end,:]
 # r1s, r2s = fem_pts[1:end-1], fem_pts[2:end]
-# M1s         = @. r1s × half_forces
-# M2s         = @. r2s × half_forces
+# M_ins    = @. r1s × half_forces
+# M_outs   = @. r2s × half_forces
 
 ## Need to check if adjacent_joiner is the right definition...
 pt_forces   = adjacent_joiner(half_forces, half_forces)
-pt_moments  = adjacent_joiner(M1s, M2s)
+pt_moments  = adjacent_joiner(M_ins, M_outs)
 
 # Matrix version
-pt_loads = zeros(length(half_forces) + 1, 6)
-test_force = permutedims(reduce(hcat, half_forces))
-pt_loads[1:end-1,1:3] = test_force
-pt_loads[2:end,1:3]  += test_force
+fem_loads  = zeros(6, length(pt_forces))
+fem_forces = reduce(hcat, half_forces)
+fem_loads[1:3,1:end-1] = fem_forces
+fem_loads[1:3,2:end]  += fem_forces
 
-test_M1s = permutedims(reduce(hcat, M1s))
-test_M2s = permutedims(reduce(hcat, M2s))
-pt_loads[1:end-1,4:end] = test_M1s
-pt_loads[2:end,4:end]  += test_M2s
-pt_loads[end:-1:6,:]
+fem_M_ins  = reduce(hcat, M_ins)
+fem_M_outs = reduce(hcat, M_outs)
+fem_loads[4:end,1:end-1] = fem_M_ins
+fem_loads[4:end,2:end]  += fem_M_outs
 
+# Constrain the plane of symmetry and print
+fem_loads[:,span_num+1] .= 0.
+fem_loads' #[:,1:span_num+1]
+ 
 ## Transform everything to principal axes
 wing_pans = (make_panels ∘ coordinates)(wing)[:]
 cs        = diff.(bound_leg.(wing_pans), dims = 1)
@@ -133,13 +142,13 @@ zero_vec = [SVector(0,0,0.)]
 Ls    = norm.(diff(fem_pts))
 
 # NEED TO CHECK THE DIRECTIONS
-left_forces   = @view pt_forces[1:middle_index(pt_forces)-1]
-left_moments  = @view pt_moments[1:middle_index(pt_moments)-1]
+left_forces   = @views pt_forces[1:middle_index(pt_forces)-1]
+left_moments  = @views pt_moments[1:middle_index(pt_moments)-1]
 right_forces  = @views [ zero_vec; pt_forces[middle_index(pt_forces)+1:end] ]
 right_moments = @views [ zero_vec; pt_moments[middle_index(pt_moments)+1:end] ]
 
-F_S = map(x -> [ x[1] * vec for vec in x[2] ], zip(dircos, [left_forces,  right_forces ]))
-M_S = map(x -> [ x[1] * vec for vec in x[2] ], zip(dircos, [left_moments, right_moments]))
+F_S = map(x -> map(y -> x[1] * y, x[2]), zip(dircos, [left_forces,  right_forces ]))
+M_S = map(x -> map(y -> x[1] * y, x[2]), zip(dircos, [left_moments, right_moments]))
 
 ## Structural setup
 #==========================================================================================#
@@ -173,6 +182,21 @@ function assemble_fem_dynamics(pt_forces, pt_moments)
     F  = [ py; pz; px ]
 end
 
+function assemble_fem_dynamics(loads)
+    Fx = loads[:,1]
+    Fy = loads[:,2] 
+    Fz = loads[:,3]
+    Mx = loads[:,4]
+    My = loads[:,5]
+    Mz = loads[:,6]
+
+    py = (collect ∘ Iterators.flatten ∘ zip)(Fy, My)
+    pz = (collect ∘ Iterators.flatten ∘ zip)(Fz, Mz)
+    px = [ Fx[:]; Mx[:] ]
+
+    F  = [ py; pz; px ]
+end
+
 Fx = getindex.(pt_forces,  1)
 Fy = getindex.(pt_forces,  2) 
 Fz = getindex.(pt_forces,  3)
@@ -183,13 +207,14 @@ Mz = getindex.(pt_moments, 3)
 df = DataFrame([ Fx Fy Fz Mx My Mz ], :auto)
 rename!(df, [:Fx, :Fy, :Fz, :Mx, :My, :Mz])
 
-py = (collect ∘ Iterators.flatten ∘ zip)(Fy, My)
-pz = (collect ∘ Iterators.flatten ∘ zip)(Fz, Mz)
-px = [ Fx[:]; Mx[:] ]
+# py = (collect ∘ Iterators.flatten ∘ zip)(Fy, My)
+# pz = (collect ∘ Iterators.flatten ∘ zip)(Fz, Mz)
+# px = [ Fx[:]; Mx[:] ]
 
 ## "FEM" setup
-K = tube_stiffness_matrix(aluminum, tubes)
-F = reduce(vcat, assemble_fem_dynamics.(F_S, M_S))
+K  = tube_stiffness_matrix(aluminum, tubes)
+Fs = assemble_fem_dynamics.(F_S, M_S)
+F  = reduce(vcat, Fs)
 
 ## Solve system(s)
 xs = K \ F
