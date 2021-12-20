540a05e129fdce64e7825435673010243c4f93cb
GodotMisogi <GodotMisogi@github.com>
2021-12-20 17:58:07 +0800

    Generated new example cases

--- examples/vortex_lattice_method/vlm_aircraft.jl
+++ examples/vortex_lattice_method/vlm_aircraft.jl
@@ -1,19 +1,25 @@
-## Aircraft analysis case
+## Wing analysis case
+using Revise
 using AeroMDAO
 using ComponentArrays
-using TimerOutputs
-## Lifting surfaces
+using LinearAlgebra
+using SplitApplyCombine
+
+## Surfaces
 
 # Wing
-wing = Wing(foils     = Foil.(fill(naca4((0,0,1,2)), 3)),
-            chords    = [1.0, 0.6, 0.2],
-            twists    = [2.0, 0.0, -2.0],
-            spans     = [4.0, 0.2],
-            dihedrals = [5., 30.],
-            LE_sweeps = [5., 30.]);
+wing = Wing(foils     = Foil.(fill(naca4(2,4,1,2), 2)),
+            chords    = [1.0, 0.6],
+            twists    = [2.0, 0.0],
+            spans     = [4.0],
+            dihedrals = [5.],
+            LE_sweeps = [5.]);
+
+x_w, y_w, z_w = wing_mac = mean_aerodynamic_center(wing)
+S, b, c = projected_area(wing), span(wing), mean_aerodynamic_chord(wing);
 
 # Horizontal tail
-htail = Wing(foils     = Foil.(fill(naca4((0,0,1,2)), 2)),
+htail = Wing(foils     = Foil.(fill(naca4(0,0,1,2), 2)),
              chords    = [0.7, 0.42],
              twists    = [0.0, 0.0],
              spans     = [1.25],
@@ -24,7 +30,7 @@ htail = Wing(foils     = Foil.(fill(naca4((0,0,1,2)), 2)),
              axis      = [0., 1., 0.])
 
 # Vertical tail
-vtail = HalfWing(foils     = Foil.(fill(naca4((0,0,0,9)), 2)),
+vtail = HalfWing(foils     = Foil.(fill(naca4(0,0,0,9), 2)),
                  chords    = [0.7, 0.42],
                  twists    = [0.0, 0.0],
                  spans     = [1.0],
@@ -39,151 +45,218 @@ print_info(wing, "Wing")
 print_info(htail, "Horizontal Tail")
 print_info(vtail, "Vertical Tail")
 
-## Assembly
-wing_panels, wing_normals  = panel_wing(wing,                 # Wing or HalfWing type
-                                        [20, 3],              # Number of spanwise panels for half-wing
-                                        10;                   # Chordwise panels
-                                        # spacing = "cosine"  # Spacing distribution: Default works well for symmetric
-                                       )
-
-htail_panels, htail_normals = panel_wing(htail, [6], 6;
-                                         spacing  = Uniform()
-                                        )
-
-vtail_panels, vtail_normals = panel_wing(vtail, [6], 5;
-                                         spacing  = Uniform()
-                                        )
-
-wing_horses  = Horseshoe.(wing_panels,  wing_normals)
-htail_horses = Horseshoe.(htail_panels, htail_normals)
-vtail_horses = Horseshoe.(vtail_panels, vtail_normals)
-
-aircraft_panels = ComponentArray(
-                                 wing  = wing_panels,
-                                 htail = htail_panels, 
-                                 vtail = vtail_panels
-                                )
+## WingMesh type
+wing_mesh  = WingMesh(wing, [12], 6, 
+                      span_spacing = Cosine()
+                     )
+htail_mesh = WingMesh(htail, [12], 6, 
+                      span_spacing = Cosine()
+                     )
+vtail_mesh = WingMesh(vtail, [12], 6, 
+                      span_spacing = Cosine()
+                     )
 
 aircraft = ComponentArray(
-                          wing  = wing_horses,
-                          htail = htail_horses,
-                          vtail = vtail_horses
-                         )
-
-wing_mac = mean_aerodynamic_center(wing)
-x_w      = wing_mac[1]
+                          wing  = make_horseshoes(wing_mesh),
+                          htail = make_horseshoes(htail_mesh),
+                          vtail = make_horseshoes(vtail_mesh)
+                         );
 
 ## Case
-ac_name = "My Aircraft"
-S, b, c = projected_area(wing), span(wing), mean_aerodynamic_chord(wing);
+ac_name = :aircraft
 ρ       = 1.225
 ref     = [ x_w, 0., 0.]
-V, α, β = 1.0, 1.0, 0.0
+V, α, β = 15.0, 0.0, 0.0
 Ω       = [0.0, 0.0, 0.0]
 fs      = Freestream(V, α, β, Ω)
+refs    = References(S, b, c, ρ, ref)
 
-@time data =
-    solve_case(aircraft, fs;
-               rho_ref     = ρ,         # Reference density
-               r_ref       = ref,       # Reference point for moments
-               area_ref    = S,         # Reference area
-               span_ref    = b,         # Reference span
-               chord_ref   = c,         # Reference chord
-               name        = ac_name,   # Aircraft name
-               print       = true,      # Prints the results for the entire aircraft
-               print_components = true, # Prints the results for each component
-              );
+##
+@time begin 
+    data = solve_case(aircraft, fs, refs;
+                      print            = true, # Prints the results for only the aircraft
+                      print_components = true, # Prints the results for all components
+                    #   finite_core      = true
+                     );
 
-## Data access
-Γs_comp    = data.wing.circulations
-CFs        = data.wing.CFs
-CMs        = data.wing.CMs
+    # Compute dynamics
+    ax       = Stability()
+    Fs       = surface_forces(data)
+    Fs, Ms   = surface_dynamics(data; axes = ax) 
+    CFs, CMs = surface_coefficients(data; axes = ax)
 
-# Component data processing
-nf_coeffs  = nearfield(data.wing) # Nearfield coefficients
-ff_coeffs  = farfield(data.wing)  # Farfield coefficients
+    nfs = nearfield_coefficients(data)
+    ffs = farfield_coefficients(data)
 
-print_coefficients(nf_coeffs, ff_coeffs, :wing)
+    nf  = nearfield(data) 
+    ff  = farfield(data)
+end;
 
-## System data processing
-Γs   = circulations(data)                # Circulations of entire system
-nf_c = sum(nearfield_coefficients(data)) # Total nearfield force and moment coefficients
-ff_c = sum(farfield_coefficients(data))  # Total farfield force coefficients
+## Spanwise forces
+using StaticArrays
+function lifting_line_loads(panels, CFs, S)
+    CFs  = combinedimsview(CFs)
+    CDis = @views CFs[1,:,:]
+    CYs  = @views CFs[2,:,:]
+    CLs  = @views CFs[3,:,:]
 
-print_coefficients(nf_c, ff_c, ac_name)
+    area_scale  = S ./ sum(panel_area, panels, dims = 1)[:]
+    span_CDis   = sum(CDis, dims = 1)[:] .* area_scale
+    span_CYs    = sum(CYs,  dims = 1)[:] .* area_scale
+    span_CLs    = sum(CLs,  dims = 1)[:] .* area_scale
 
-## Stability case
-@time dv_data =
-    solve_stability_case(aircraft, fs;
-                         rho_ref     = ρ,
-                         r_ref       = ref,
-                         area_ref    = S,
-                         span_ref    = b,
-                         chord_ref   = c,
-                         name        = ac_name,
-                         print       = true,
-                         print_components = true,
-                        );
+    # ys = sum(x -> getindex(midpoint(x), 2), panels, dims = 1) 
 
-## Data collection
-names = (collect ∘ keys)(dv_data)
-comp  = names[1]
-nf, ff, dvs = dv_data[comp];
-print_coefficients(nf, ff, comp)
-print_derivatives(dvs, comp)
+    SVector.(span_CDis, span_CYs, span_CLs)
+end
+
+hs_pts   = horseshoe_point.(data.horseshoes)
+wing_ys  = @views getindex.(hs_pts.wing[1,:],  2)
+htail_ys = @views getindex.(hs_pts.htail[1,:], 2)
+vtail_ys = @views getindex.(hs_pts.vtail[1,:], 2)
+
+##
+wing_ll  = lifting_line_loads(chord_panels(wing_mesh), CFs.wing, S)
+htail_ll = lifting_line_loads(chord_panels(htail_mesh), CFs.htail, S)
+vtail_ll = lifting_line_loads(chord_panels(vtail_mesh), CFs.vtail, S);
+
+## Plotting
+using GLMakie
+using LaTeXStrings
+
+set_theme!(
+            # theme_black()
+            # theme_light()
+          )
+
+const LS = LaTeXString
 
 ## Streamlines
-
-# Chordwise distribution
-# using StaticArrays
-
-# num_points = 100
-# max_z = 2
-# y = span(wing) / 10
-# seed = SVector.(fill(-0.1, num_points), fill(y, num_points), range(-max_z, stop = max_z, length = num_points))
-
 # Spanwise distribution
-span_points = 10
+span_points = 30
 init        = chop_leading_edge(wing, span_points)
-dx, dy, dz  = 0, 0, 1e-3
-seed        = [ init .+ Ref([dx, dy, dz]) ;
-                init .+ Ref([dx, dy,-dz]) ];
+dx, dy, dz  = 0, 0, 1e-1
+seed        = init # [ init .+ Ref([dx, dy,  dz])
+                   #   init .+ Ref([dx, dy, -dz]) ];
 
-distance = 8
-num_stream_points = 200
-streams = plot_streams(fs, seed, aircraft, Γs, distance, num_stream_points);
+distance = 5
+num_stream_points = 100
+streams = plot_streams(fs, seed, data.horseshoes, data.circulations, distance, num_stream_points);
 
-##
-using Plots
-gr(size = (1280, 720), dpi = 300)
+## Mesh connectivities
+triangle_connectivities(inds) = @views [ inds[1:end-1,1:end-1][:] inds[1:end-1,2:end][:]   inds[2:end,2:end][:]   ;
+                                           inds[2:end,2:end][:]   inds[2:end,1:end-1][:] inds[1:end-1,1:end-1][:] ]
 
-##
-horseshoe_coords = plot_panels(aircraft_panels)
-horseshoe_points = Tuple.(horseshoe_point.(aircraft_panels));
+wing_cam_connec  = triangle_connectivities(LinearIndices(wing_mesh.cam_mesh))
+htail_cam_connec = triangle_connectivities(LinearIndices(htail_mesh.cam_mesh))
+vtail_cam_connec = triangle_connectivities(LinearIndices(vtail_mesh.cam_mesh));
 
-##
-z_limit = b
-plot(xaxis = "x", yaxis = "y", zaxis = "z",
-     camera = (30, 60),
-    #  xlim = (-z_limit/2, z_limit/2),
-     aspect_ratio = 1,
-     zlim = (-z_limit/2, z_limit/2),
-     size = (1280, 720)
-    )
-plot!.(horseshoe_coords, color = :gray, label = :none)
-scatter!(horseshoe_points, marker = 1, color = :black, label = :none)
-plot!.(streams, color = :green, label = :none)
-plot!()
+## Extrapolating surface values to neighbouring points
+function extrapolate_point_mesh(mesh)
+    m, n   = size(mesh)
+    points = zeros(eltype(mesh), m + 1, n + 1)
 
-## Exaggerated CF distribution for plot, only works with GR and not Plotly
+    # The quantities are measured at the bound leg (0.25×)
+    @views points[1:end-1,1:end-1] += 0.75 * mesh / 2
+    @views points[1:end-1,2:end]   += 0.75 * mesh / 2
+    @views points[2:end,1:end-1]   += 0.25 * mesh / 2
+    @views points[2:end,2:end]     += 0.25 * mesh / 2
 
-# Forces
-wind_CFs = body_to_wind_axes.(CFs, fs.alpha, fs.beta)
-CDis     = @. getindex(wind_CFs, 1)
-CYs      = @. getindex(wind_CFs, 2)
-CLs      = @. getindex(wind_CFs, 3)
+    points
+end
 
-hs_pts = Tuple.(bound_leg_center.(aircraft))
+## Surface velocities
+vels = surface_velocities(data);
+sps  = norm.(vels)
 
-quiver!(hs_pts, quiver=(CDis[:], CYs[:], CLs[:]) .* 500)
-plot!()
+wing_sp_points  = extrapolate_point_mesh(sps.wing)
+htail_sp_points = extrapolate_point_mesh(sps.htail)
+vtail_sp_points = extrapolate_point_mesh(sps.vtail)
+
+## Surface pressure coefficients
+cps  = norm.(CFs) * S
+
+wing_cp_points  = extrapolate_point_mesh(cps.wing)
+htail_cp_points = extrapolate_point_mesh(cps.htail)
+vtail_cp_points = extrapolate_point_mesh(cps.vtail)
+
+## Figure plot
+fig  = Figure(resolution = (1280, 720))
+
+scene = LScene(fig[1:4,1])
+ax    = fig[1:4,2] = GridLayout()
+
+ax_cd = GLMakie.Axis(ax[1,1:2], ylabel = L"C_{D_i}", title = LS("Spanwise Loading"))
+ax_cy = GLMakie.Axis(ax[2,1:2], ylabel = L"C_Y",)
+ax_cl = GLMakie.Axis(ax[3,1:2], xlabel = L"y", ylabel = L"C_L")
+
+# Spanload plot
+function plot_spanload!(ax, ys, CFs, name = "Wing")
+    CFs  = combinedimsview(CFs)
+    @views lines!(ax[1,1:2], ys, CFs[1,:], label = name,)
+    @views lines!(ax[2,1:2], ys, CFs[2,:], label = name,)
+    @views lines!(ax[3,1:2], ys, CFs[3,:], label = name,)
+
+    nothing
+end
+
+plot_spanload!(ax, wing_ys, wing_ll, LS("Wing"))
+plot_spanload!(ax, htail_ys, htail_ll, LS("Horizontal Tail"))
+plot_spanload!(ax, vtail_ys, vtail_ll, LS("Vertical Tail"))
+
+# Legend
+Legend(ax[4,1:2], ax_cl)
+fig[0, :] = Label(fig, LS("Vortex Lattice Analysis"), textsize = 20)
+
+# Surface pressure meshes
+m1 = poly!(scene, wing_mesh.cam_mesh[:],  wing_cam_connec,  color =  wing_cp_points[:])
+m2 = poly!(scene, htail_mesh.cam_mesh[:], htail_cam_connec, color = htail_cp_points[:])
+m3 = poly!(scene, vtail_mesh.cam_mesh[:], vtail_cam_connec, color = vtail_cp_points[:])
+
+# Airfoil meshes
+# wing_surf = surface_coordinates(wing_mesh, wing_mesh.n_span, 60)
+# surf_connec = triangle_connectivities(LinearIndices(wing_surf))
+# wing_surf_mesh = mesh(wing_surf[:], surf_connec)
+# w1 = wireframe!(scene, wing_surf_mesh.plot[1][], color = :grey, alpha = 0.1)
+
+# Borders
+lines!(scene, plot_wing(wing))
+lines!(scene, plot_wing(htail))
+lines!(scene, plot_wing(vtail))
+
+l1 = [ lines!(scene, pts, color = :grey) for pts in plot_panels(make_panels(wing_mesh.cam_mesh))  ]
+l2 = [ lines!(scene, pts, color = :grey) for pts in plot_panels(make_panels(htail_mesh.cam_mesh)) ]
+l3 = [ lines!(scene, pts, color = :grey) for pts in plot_panels(make_panels(vtail_mesh.cam_mesh)) ]
+
+# Streamlines
+[ lines!(scene, stream[:], color = :green) for stream in eachcol(streams) ]
+
+fig.scene
+
+## Save figure
+# save("plots/VortexLattice.png", fig, px_per_unit = 1.5)
+
+## Animation settings
+pts = [ Node(Point3f0[stream]) for stream in streams[1,:] ]
+
+[ lines!(scene, pts[i], color = :green, axis = (; type = Axis3)) for i in eachindex(pts) ]
+
+# Recording
+fps     = 30
+nframes = length(streams[:,1])
+
+record(fig, "plots/vlm_animation.mp4", 1:nframes) do i 
+    for j in eachindex(streams[1,:])
+        pts[j][] = push!(pts[j][], Point3f0(streams[i,j]))
+    end
+    sleep(1/fps) # refreshes the display!
+    notify(pts[i])
+end
+
+## Arrows
+# hs_pts = Tuple.(bound_leg_center.(horses))[:]
+# arrows!(scene, getindex.(hs_pts, 1), getindex.(hs_pts, 2), getindex.(hs_pts, 3), 
+#                 CDis[:], CYs[:], CLs[:], 
+#                 arrowsize = Vec3f.(0.3, 0.3, 0.4),
+#                 lengthscale = 10,
+#                 label = "Forces (Exaggerated)")
--- examples/vortex_lattice_method/vlm_mesh_tests.jl
+++ examples/vortex_lattice_method/vlm_mesh_tests.jl
@@ -1,7 +1,7 @@
 ## Mesh convergence tests
 using AeroMDAO
 using Plots
-using DataFrames, StatsPlots
+using DataFrames
 
 ## Wing
 wing = Wing(foils     = Foil.(fill(naca4((0,0,1,2)), 2)),
@@ -38,20 +38,25 @@ panel_case(x, y) = solve_case(wing, fs;
 xs = 10:2:50
 ys = xs .* 2# fill(maximum(xs) * 4, length(xs))
 
-nf_data, ff_data = zeros(5)', zeros(5)'
-for (x, y) in zip(xs, ys)
+num = length(xs)
+
+coeff_data = zeros(num, 9 + 2)
+for (i, (x, y)) in enumerate(zip(xs, ys))
     @time nf_coeffs, ff_coeffs, CFs, CMs, horseshoe_panels, camber_panels, horseshoes, Γs =
     panel_case(x, y)
 
     println("Distribution: nx = $x, ny = $y")
     print_coefficients(nf_coeffs, ff_coeffs)
-    nf_data = [ nf_data; [x; y; nf_coeffs[1:3] ]' ]
-    ff_data = [ ff_data; [x; y; ff_coeffs[1:3] ]' ]
+
+    coeff_data[i,1]     = x
+    coeff_data[i,2]     = y
+    coeff_data[i,3:5]   = ff_coeffs
+    coeff_data[i,6:end] = nf_coeffs
 end
 
 ##
-data = DataFrame([ nf_data ff_data[:,3:end] ], :auto)
-rename!(data, [:chord_num, :span_num, :CD_nf, :CY_nf, :CL_nf, :CD_ff, :CY_ff, :CL_ff])
+data = DataFrame(coeff_data, :auto)
+rename!(data, [:chord_num, :span_num, :CD_ff, :CY_ff, :CL_ff, :CD_nf, :CY_nf, :CL_nf, :Cl, :Cm, :Cn])
 
 ##
 errors = @. abs((data[3:end,3:end] - data[2:end-1,3:end]) / data[2:end-1,3:end])
@@ -59,19 +64,31 @@ errors = @. abs((data[3:end,3:end] - data[2:end-1,3:end]) / data[2:end-1,3:end])
 ##
 plot(xlabel = "Chordwise Panels", ylabel = "Error", yscale = :log10)
 
-# CDs
+# CDs Errors
 plot!(data[2:end-1,"chord_num"], errors[!,"CD_nf"], label = "CD Nearfield")
 plot!(data[2:end-1,"chord_num"], errors[!,"CD_ff"], label = "CD Farfield")
 
-# CLs
+# CLs Errors
 plot!(data[2:end-1,"chord_num"], errors[!,"CL_nf"], label = "CL Nearfield")
 plot!(data[2:end-1,"chord_num"], errors[!,"CL_ff"], label = "CL Farfield")
 
-# CYs
+# CYs Errors
 plot!(data[2:end-1,"chord_num"], errors[!,"CY_nf"], label = "CY Nearfield")
 plot!(data[2:end-1,"chord_num"], errors[!,"CY_ff"], label = "CY Farfield")
 
 ##
-plot()
-plot!(data[2:end,"chord_num"], data[2:end,"CL_nf"], marker = :dot, label = "Nearfield")
+CL = plot(data[2:end,"chord_num"], data[2:end,"CL_nf"], marker = :dot, label = "Nearfield", ylabel = "CL")
 plot!(data[2:end,"chord_num"], data[2:end,"CL_ff"], marker = :dot, label = "Farfield")
+
+CD = plot(data[2:end,"chord_num"], data[2:end,"CD_nf"], marker = :dot, label = "Nearfield", ylabel = "CD")
+plot!(data[2:end,"chord_num"], data[2:end,"CD_ff"], marker = :dot, label = "Farfield")
+
+CY = plot(data[2:end,"chord_num"], data[2:end,"CY_nf"], marker = :dot, label = "Nearfield", ylabel = "CY")
+plot!(data[2:end,"chord_num"], data[2:end,"CY_ff"], marker = :dot, label = "Farfield")
+
+Cl = plot(data[2:end,"chord_num"], data[2:end,"Cl"], marker = :dot, label = "Nearfield", ylabel = "Cl")
+Cm = plot(data[2:end,"chord_num"], data[2:end,"Cm"], marker = :dot, label = "Nearfield", ylabel = "Cm")
+Cn = plot(data[2:end,"chord_num"], data[2:end,"Cn"], marker = :dot, label = "Nearfield", ylabel = "Cn")
+
+
+plot(CL, CD, CY, Cl, Cm, Cn, layout = (2,3), size = (1280, 720))
