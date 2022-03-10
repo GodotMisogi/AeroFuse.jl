8c8117bfa3858ffe21ed7038c7646f37661488af
GodotMisogi <GodotMisogi@github.com>
2022-03-10 14:31:34 +0800

    Added trailing edge info

--- src/Geometry/PanelGeometry/2d_panels.jl
+++ src/Geometry/PanelGeometry/2d_panels.jl
@@ -22,13 +22,13 @@ WakePanel2D(p1 :: FieldVector{2,T}, p2 :: FieldVector{2,T}) where T <: Real = Wa
 a :: AbstractPanel2D + b :: AbstractPanel2D = Panel2D(p1(a) + p1(b), p2(a) + p2(b))
 a :: AbstractPanel2D - b :: AbstractPanel2D = Panel2D(p1(a) - p1(b), p2(a) - p2(b))
 
-collocation_point(panel :: AbstractPanel2D, a = 0.5) = a * (p1(panel) + p2(panel))
-panel_vector(panel :: AbstractPanel2D) = p2(panel) - p1(panel)
+collocation_point(panel :: AbstractPanel2D, a = 0.5) = a * (panel.p1 + panel.p2)
+panel_vector(panel :: AbstractPanel2D) = panel.p2 - panel.p1
 panel_length(panel :: AbstractPanel2D) = (norm ∘ panel_vector)(panel)
 
 function transform_panel_points(panel_1 :: AbstractPanel2D, panel_2 :: AbstractPanel2D)
-    x1, y1, x2, y2 = p1(panel_2), p2(panel_2)
-    xs, ys = p1(panel_1)
+    x1, y1, x2, y2 = panel_2.p1, panel_2.p2
+    xs, ys = panel_1.p1
 
     xp1, yp1 = affine_2D(x1, y1, xs, ys, panel_angle(panel_1))
     xp2, yp2 = affine_2D(x2, y2, xs, ys, panel_angle(panel_1))
@@ -36,12 +36,18 @@ function transform_panel_points(panel_1 :: AbstractPanel2D, panel_2 :: AbstractP
     xp1, yp1, xp2, yp2
 end
 
-function transform_panel(panel :: AbstractPanel2D, point :: SVector{2,<: Real})
-    xs, ys = p1(panel)
-    affine_2D(first(point), last(point), xs, ys, panel_angle(panel))
+function transform_panel(panel :: AbstractPanel2D, x, y)
+    xs, ys = panel.p1
+    affine_2D(x, y, xs, ys, panel_angle(panel))
 end
 
-panel_angle(panel :: AbstractPanel2D) = let (x, y) = panel_vector(panel); atan(y, x) end
+transform_panel(panel :: AbstractPanel2D, point :: SVector{2,<: Real}) = transform_panel(panel, point.x, point.y)
+
+function panel_angle(panel :: AbstractPanel2D)
+    (x, y) = panel_vector(panel)
+    θ = atan(y, x)
+    # ifelse(θ < 0., θ + 2π, θ) # Branch cut
+end
 
 panel_normal(panel :: AbstractPanel2D) = let (x, y) = panel_vector(panel); [-y; x] end
 
@@ -56,6 +62,26 @@ panel_points(panels) = [ p1.(panels); [(p2 ∘ last)(panels)] ]
 reverse_panel(panel :: AbstractPanel2D) = Panel2D(panel.p2, panel.p1)
 
 trailing_edge_panel(panels) = Panel2D((p2 ∘ last)(panels), (p1 ∘ first)(panels))
+
+bisector(f_pan, l_pan) = (normalize(panel_vector(l_pan)) + normalize(-panel_vector(f_pan))) / 2
+
+function trailing_edge_info(panels)
+    # Make trailing edge
+    te_panel  = trailing_edge_panel(panels)
+    s    = panel_vector(reverse_panel(te_panel))
+    p    = normalize(s)
+
+    t1 = -normalize(panel_vector(panels[1]))
+    t2 = normalize(panel_vector(panels[end]))
+    t  = normalize((t1 + t2) / 2)
+
+    h_TE = -s[1] * t[2] + s[2] * t[1]
+    tcp  = abs(t[1] * p[2] - t[2] * p[1])
+    tdp  = dot(p, t)
+    dtdx = t1[1] * t2[2] - t2[1] * t1[2]
+
+    te_panel, h_TE, tcp, tdp, dtdx
+end
 
 function wake_panel(panels, bound, α)
     _, firsty        = (p1 ∘ first)(panels)
