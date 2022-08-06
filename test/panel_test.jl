8aa7766cc4f864b710e850e437a74dfa8a468ebb
jamesmshihua <64199215+jamesmshihua@users.noreply.github.com>
2022-08-06 18:49:39 +0800

    3D panel method finished but not validated

--- test/panel_test.jl
+++ test/panel_test.jl
@@ -11,3 +11,7 @@ point = Point3D(0.0, 0.0, 10.0)
 
 thispan = surf_pans[1,1]
 plot(xs(thispan), ys(thispan), zs(thispan), xs(thispan))
+
+plot!(xs(foil_pans[npansp+1]), ys(foil_pans[npansp+1]), zs(foil_pans[npansp+1]), dpi=300, xlabel="x", ylabel="y", zlabel="z")
+plot!(xs(foil_pans[npansp+2]), ys(foil_pans[npansp+2]), zs(foil_pans[npansp+2]), dpi=300, xlabel="x", ylabel="y", zlabel="z")
+plot!(xs(foil_pans[npansp+3]), ys(foil_pans[npansp+3]), zs(foil_pans[npansp+3]), dpi=300, xlabel="x", ylabel="y", zlabel="z")
