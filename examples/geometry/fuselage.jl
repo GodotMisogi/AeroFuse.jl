fea139cad2d09426a8743fa44dc61f22565fad3b
Arjit Seth <arjitseth@gmail.com>
2023-03-27 18:19:40 +0800

    Fixed fuselage surface area bug and updated docs and tuts

--- examples/geometry/fuselage.jl
+++ examples/geometry/fuselage.jl
@@ -1,20 +1,36 @@
 ## Fuselage example
 using AeroFuse
+using Plots
 
 # Fuselage parameters
-l_fuselage = 18.      # Length (m)
-h_fuselage = 1.5      # Height (m)
-w_fuselage = 1.8      # Width (m)
-S_fuselage = 90       # Surface area (m)²
-K_fuselage = 0.487    # Fuselage parameter
+l_fuselage = 18. # Length (m)
+h_fuselage = 1.5 # Height (m)
+w_fuselage = 1.8 # Width (m)
 
-# Chordwise locations and corresponding radii
+## Hyperelliptic fuselage
+fuse = HyperEllipseFuselage(
+    radius = w_fuselage / 2,
+    length = l_fuselage,
+    c_nose = 2,
+    c_rear = 2,
+)
+
+ts = 0:0.1:1                # Distribution of sections
+S_f = wetted_area(fuse, ts) # Surface area, m²
+V_f = volume(fuse, ts)      # Volume, m³
+
+## Plot
+plot(fuse, 
+    aspect_ratio = 1, 
+    zlim = (-0.5, 0.5) .* fuse.length,
+    label = "Fuselage"
+)
+
+## Chordwise locations and corresponding radii
 lens = [0.0, 0.005, 0.01, 0.03, 0.1, 0.2, 0.4, 0.6, 0.7, 0.8, 0.98, 1.0]
 rads = [0.05, 0.15, 0.25, 0.4, 0.8, 1., 1., 1., 1., 0.85, 0.3, 0.01] * w_fuselage / 2
 
 fuse = Fuselage(l_fuselage, lens, rads, [0., 0., 0.])
 
 ## Plotting
-using Plots
-
 plot(fuse, aspect_ratio = 1, zlim = (-10,10))
